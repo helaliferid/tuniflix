da2579bb2869ba254142f360a672da6929c88600
Ferid HELALI <ferid.helali@upgrade-code.org>
2022-01-10 19:07:43 +0100

    data event binding

--- ngapp/src/app/app.component.ts
+++ ngapp/src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ngapp';
+  public movie:any='';
+
+  setMovie(_movie:{title:string,year:string}){
+    this.movie=_movie
+  }
+
+  log(){
+    console.log(this.movie)
+  }
+
 }
