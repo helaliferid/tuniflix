0e6a6268e84a467846f84ebdc670255c5f4b4f7f
Ferid HELALI <ferid.helali@upgrade-code.org>
2022-01-11 17:54:11 +0100

    scaffold the movie catalog

--- ngapp/src/app/movies/movies-catalog/movies-catalog.component.ts
+++ ngapp/src/app/movies/movies-catalog/movies-catalog.component.ts
@@ -6,7 +6,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./movies-catalog.component.scss']
 })
 export class MoviesCatalogComponent implements OnInit {
-
+  movies = [
+    {
+      title: 'The Oval',
+      description: 'Action Movie',
+      cover: 'https://fr.web.img4.acsta.net/c_310_420/pictures/19/10/16/16/53/3090773.jpg'
+    },
+    {
+      title: 'Yori Boy',
+      description: 'Action Movie MMA',
+      cover: 'https://upload.wikimedia.org/wikipedia/en/b/b4/Boyka_Undisputed.jpg'
+    },
+    {
+      title: 'Casa de Papel',
+      description: 'Action Movie',
+      cover: 'https://wiflix.biz/wfimages/stream-vf-5f5f-f80a-9cd1-4440.jpg'
+    },
+    {
+      title: 'Free Fight',
+      description: 'Action Movie Martial Art',
+      cover: 'https://wiflix.biz/wfimages/stream-vf-5f5f-f80a-9cd1-4440.jpg'
+    },
+    {
+      title: 'Pearl Harobour',
+      description: 'Japan agains USA 2 WW War',
+      cover: 'https://metro.co.uk/wp-content/uploads/2021/12/pearl-harbor-movie-511e.jpg?quality=90&strip=all&zoom=1&resize=644%2C483'
+    },
+  ]
   constructor() { }
 
   ngOnInit(): void {
